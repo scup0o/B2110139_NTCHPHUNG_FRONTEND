eda8c2e959a6632b910004c4b7030c4093636374
scup0o <haiphung0015@gmail.com>
2023-10-07 14:28:18 +0700

    Cai dat trang loi 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="page">
+        <p>
+            Oops, Không thể tìm thấy trang. Trở về
+            <router-link to="/">trang chủ.</router-link>
+        </p>
+    </div>
+</template>
